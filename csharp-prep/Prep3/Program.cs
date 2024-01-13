226895584f709c1b8ebda32c6146d0ecce8e9187
josArt17 <josart1296@gmail.com>
2024-01-13 09:38:44 -0600

    stretchallenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int number = random.Next(1, 101);
+        int tries = 0;
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Console.WriteLine("Welcome guess the number");
+            Console.WriteLine();
+            Console.Write("Write a number: ");
+            double response = Convert.ToDouble(Console.ReadLine());
+
+            while (response != number)
+            {
+                tries++;
+                if (response > number)
+                {
+                    Console.WriteLine("Lower");
+                    Console.WriteLine();
+                    Console.Write("Write a number: ");
+                    response = Convert.ToDouble(Console.ReadLine());
+                }
+                else if (response < number)
+                {
+                    Console.WriteLine("Higher");
+                    Console.WriteLine();
+                    Console.Write("Write a number: ");
+                    response = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            tries++;
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You had {tries} tries");
+            Console.WriteLine();
+            Console.Write("Do you wanna play again? ");
+            string again = Console.ReadLine().ToLower();
+
+            if (again == "yes")
+            {
+                playAgain = true;
+                number = random.Next(1, 101);
+            }
+            else
+            {
+                playAgain = false;
+                Console.WriteLine("See you");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        bool finish = false;
+
+        while (finish == false)
+        {
+            Console.Write("Enter number, type 0 when you finish: ");
+            int number;
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    finish = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        numbers.Sort();
+
+        int sum = 0;
+        int lNumber = int.MinValue;
+        int mNumber = int.MaxValue;
+
+        foreach (int value in numbers)
+        {
+            sum += value;
+            if (value > lNumber)
+            {
+                lNumber = value;
+            }
+            if (value < mNumber && value > 0)
+            {
+                mNumber = value;
+            }
+        }
+
+        float avg = sum / numbers.Count;
+
+        
+        
+
+
+
+        Console.Write($"The sum is: {sum}\n");
+        Console.Write($"The average is: {avg}\n");
+        Console.Write($"The largest number is: {lNumber}\n");
+        Console.Write($"The smallest positive number is: {mNumber}\n");
+        Console.Write("The sorted list is:\n");
+
+        foreach (int item in numbers)
+        {
+            Console.Write($"{item}\n");
+        }
     }
 }
